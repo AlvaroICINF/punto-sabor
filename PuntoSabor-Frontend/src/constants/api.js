6e46a898bfb95769429827378d0f785ddc4a22a9
AlvaroICINF <alvaroibanez.icinf@gmail.com>
2025-06-17 11:06:41 -0400

    update

--- PuntoSabor-Frontend/src/constants/api.js
+++ PuntoSabor-Frontend/src/constants/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Configuración base de la API
-const API_BASE_URL = "http://localhost:3000/api/v1";
+const API_BASE_URL = "https://punto-sabor-backend.vercel.app/api/v1";
 
 // Crear instancia de axios
 const apiClient = axios.create({
